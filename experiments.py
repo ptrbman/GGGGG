ca6d287601b84cd0e10c99381ac6ad705ffe5086
Peter Backeman <peter@backeman.se>
2021-05-17 21:10:09 +0200

    Update experiments

--- experiments.py
+++ experiments.py
@@ -17,8 +17,9 @@ vtaloc = "/Applications/uppaal64-4.1.24/bin-Darwin/verifyta"
 executors = 4
 queue_length = 4
 instanceName = sys.argv[1]
+step = int(sys.argv[2])
 print("Loading: " + instanceName)
-
+results = []
 
 def run(sysdict):
     start_time = time.time()
@@ -27,78 +28,81 @@ def run(sysdict):
     total_time = end_time - start_time
     return (bound, answer, total_time)
 
-#### 1] Increase activation count for each node ####
-print("Running max instantiations from " + str(LB1) + " to " + str(UB1))
 
-# (BOUND, ANSWER, TIME)
-results1 = []
+if step == 1:
+    #### 1] Increase activation count for each node ####
+    print("Running max instantiations from " + str(LB1) + " to " + str(UB1))
 
-for bound in range(LB1, UB1+1):
-    print("Max insantiations: " + str(bound))
-    sysdict = LoadSystem(instanceName)
-    UE = sysdict['UserEquipments']
-    for ue in UE:
-        ue.maxInst = bound
+    # (BOUND, ANSWER, TIME)
+    results1 = []
 
-    sysdict['Executors'] = bound*len(UE)
-    sysdict['QueueLength'] = bound*len(UE)
+    for bound in range(LB1, UB1+1):
+        print("Max insantiations: " + str(bound))
+        sysdict = LoadSystem(instanceName)
+        UE = sysdict['UserEquipments']
+        for ue in UE:
+            ue.maxInst = bound
 
-    results1.append(run(sysdict))
+        sysdict['Executors'] = bound*len(UE)
+        sysdict['QueueLength'] = bound*len(UE)
 
-##### 2] Increase number of user equipment
-results2 = []
+        results1.append(run(sysdict))
+    results.append(results1)
 
+if step == 2:
 
-for bound in range(LB2, UB2+1):
-    print("Copies of UEs: " + str(bound))
-    sysdict = LoadSystem(instanceName)
-    UE = sysdict['UserEquipments']
-
-    newUE = []
-    for ue in UE:
-        for _ in range(0, bound):
-            newUE.append(UserEquipment(-1, int(ue.maxInst), int(ue.actTime), int(ue.subscribedSlice)))
-
-    sysdict['UserEquipments'] = UE + newUE
-    sysdict['Executors'] = bound*len(UE)
-    sysdict['QueueLength'] = bound*len(UE)
-
-    results2.append(run(sysdict))
+    ##### 2] Increase number of user equipment
+    results2 = []
 
 
+    for bound in range(LB2, UB2+1):
+        print("Copies of UEs: " + str(bound))
+        sysdict = LoadSystem(instanceName)
+        UE = sysdict['UserEquipments']
 
-### 3] Increase number of Executors
-results3 = []
+        newUE = []
+        for ue in UE:
+            for _ in range(0, bound):
+                newUE.append(UserEquipment(-1, int(ue.maxInst), int(ue.actTime), int(ue.subscribedSlice)))
 
-for bound in range(LB3, UB3+1):
-    print("Executors: " + str(bound))
-    sysdict = LoadSystem(instanceName)
-    sysdict['Executors'] = bound
-    sysdict['QueueLength'] = 4
+        sysdict['UserEquipments'] = UE + newUE
+        sysdict['Executors'] = bound*len(UE)
+        sysdict['QueueLength'] = bound*len(UE)
 
-    results3.append(run(sysdict))
+        results2.append(run(sysdict))
+    results.append(results2)
 
-for (b, a, t) in results3:
-    if a:
-        res = "SAT"
-    else:
-        res = "UNSAT"
-    print(str(b) + "\t" + res + "\t" + str(t))
+if step == 3:
+    ### 3] Increase number of Executors
+    results3 = []
 
-### 4] Increase queue length 
-results4 = []
+    for bound in range(LB3, UB3+1):
+        print("Executors: " + str(bound))
+        sysdict = LoadSystem(instanceName)
+        sysdict['Executors'] = bound
+        sysdict['QueueLength'] = 4
 
-for bound in range(LB4, UB4+1):
-    print("Queue Length: " + str(bound))
-    sysdict = LoadSystem(instanceName)
-    sysdict['Executors'] = executors
-    sysdict['QueueLength'] = bound
+        results3.append(run(sysdict))
+    results.append(results3)
 
-    results4.append(run(sysdict))
+if step == 4:
+    ### 4] Increase queue length 
+    results4 = []
 
-for results in [results1, results2, results3, results4]:
+    for bound in range(LB4, UB4+1):
+        print("Queue Length: " + str(bound))
+        sysdict = LoadSystem(instanceName)
+        sysdict['Executors'] = executors
+        sysdict['QueueLength'] = bound
+
+        results4.append(run(sysdict))
+    results.append(results4)
+
+print(results)
+
+for rs in results:
     print("RESULTS")
-    for (b, a, t) in results:
+    for (b, a, t) in rs:
         if a:
             res = "SAT"
         else:
